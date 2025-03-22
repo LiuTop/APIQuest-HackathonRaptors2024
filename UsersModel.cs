a1a9ceddda518a0b1880ae9d69a40d99a9698fde
LiuTop <137952103+LiuTop@users.noreply.github.com>
2025-03-22 18:55:43 +0300

    Add files via upload

---
+++ UsersModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace QAHackathon.BussinesObjects.Models
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class UsersModel
+    {
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+
+        [JsonProperty("users")]
+        public ICollection<UserModel> Users { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public partial class Meta
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
+}
