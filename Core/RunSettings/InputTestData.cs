50451429eb77baf9d61adde936259b5bc86f16cd
LiuTop <137952103+LiuTop@users.noreply.github.com>
2025-03-22 19:15:19 +0300

    Add files via upload

---
+++ Core/RunSettings/InputTestData.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace QAHackathon.Core.RunSettings
+{
+    public class InputTestData
+    {
+        [JsonProperty("api")]
+        public Api Api { get; set; }
+    }
+    public class Api
+    {
+        [JsonProperty("users")]
+        public Users Users { get; set; }
+    }
+
+    public class Users
+    {
+        [JsonProperty("getallusers")]
+        public List<string> GetAllUsers { get; set; }
+
+        [JsonProperty("createuser")]
+        public List<string> CreateUser { get; set; }
+
+        [JsonProperty("updateuser")]
+        public List<string> UpdateUser { get; set; }
+
+        [JsonProperty("getuserbyuuid")]
+        public string GetUserByUuid { get; set; }
+
+        [JsonProperty("deleteuser")]
+        public string DeleteUser { get; set; }
+
+        [JsonProperty("getuserbypassandemail")]
+        public string GetUserByPassAndEmail { get; set; }
+    }
+}
---
+++ Core/RunSettings/SetUpSettings.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace QAHackathon.Core.RunSettings
+{
+    public class SetUpSettings
+    {
+        public string releaseEndpoint;
+        public string devEndpoint;
+        public string email;
+        public string environment;
+        public bool setupEnvironmentRelease;
+        public bool setupEnvironmentDev;
+
+        public SetUpSettings()
+        {
+            releaseEndpoint = TestContext.Parameters.Get("ReleaseEndpoint");
+            devEndpoint = TestContext.Parameters.Get("DevEndpoint");
+            email = TestContext.Parameters.Get("Email");
+            environment = TestContext.Parameters.Get("Environment");
+        }
+
+        public SetupModel GetSetupSettings()
+        {
+            var config = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Core/RunSettings", "config.json"));
+
+            return JsonConvert.DeserializeObject<SetupModel>(config);
+        }
+
+        public void ChangeSetupConfig(SetupModel setupModel)
+        {
+            var newConfig = JsonConvert.SerializeObject(setupModel, Formatting.Indented);
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Core/RunSettings", "config.json"), newConfig);
+        }
+    }
+}
